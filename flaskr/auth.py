78bd72e001bb598d6163745f26d61a00e7e03b67
Maciej Krzyżowski <macikrz683@student.polsl.pl>
2022-12-05 19:33:04 +0100

    almost got login to work

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -55,7 +55,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         error = None
-        user = User.query.filter_by(username).first()
+        user = User.query.filter_by(username = username).first()
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
