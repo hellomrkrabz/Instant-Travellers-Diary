03673d8c2603f4da470043ff2872e8849077d9d7
Maciej Krzyżowski <macikrz683@student.polsl.pl>
2022-12-05 19:18:42 +0100

    trying to make db.create_all() to work

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -42,3 +42,4 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     return app
+
--- flaskr/user.py
+++ flaskr/user.py
@@ -2,6 +2,10 @@ from profile import Profile
 from . import db
 from datetime import datetime
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import config
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +20,22 @@ class User(db.Model):
     # def __init__(self, username: str, profile: Profile):
     #     self.username = username
     #     self.profile = profile
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(config['development'])
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+    db.session.add(User(username = 'test1', email = 'test1@example.com', password_hash = "test"))
+    db.session.add(User(username = 'test2', email = 'test2@example.com', password_hash = "test"))
+
+    db.session.commit()
+
+    users = User.query.all()
+    print(users)
