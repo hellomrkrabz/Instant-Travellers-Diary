733277cf5cb4667eddb3879a322c9c48db009338
suaviq <alasen24@gmail.com>
2022-12-05 18:47:07 +0100

    not working version

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,6 @@
 import functools
-
+from .user import User
+from . import db
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
@@ -12,12 +13,10 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    pass
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        db = get_db()
         error = None
 
         if not username:
@@ -29,13 +28,22 @@ def register():
 
         if error is None:
             try:
-                db.execute(
-                    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-                    (username, email, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError as regError:
-                errMsg = str(regError)
+                # user = User(username = username,
+                #             email = email, 
+                #             password_hash = generate_password_hash(password))
+                user = User(username = "abcdef",
+                            email = "abd@abd.abd",
+                            password_hash = generate_password_hash("abc"))
+                db.session.add(user)
+                db.session.commit()
+                # db.execute(
+                #     "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                #     (username, email, generate_password_hash(password)),
+                # )
+                # db.commit()
+            except Exception as e:
+                errMsg = str(e)
+                print(errMsg)
                 error = ""
                 if errMsg.endswith('user.email'):
                     error = "E-mail {email} is already taken"
@@ -53,16 +61,11 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    pass
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM users WHERE username = ?', (username,)
-        ).fetchone()
-
+        user = User.query.filter_by(username).first()
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
@@ -81,13 +84,11 @@ def login():
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-
+    
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM users WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = User.query.filter_by(user_id).first()
 
 
 @bp.route('/logout')
