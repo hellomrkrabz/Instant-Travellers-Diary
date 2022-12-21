fe811cb8b69177acb0bdc0387bccd6a80a4ae1d1
Zuucker <kamigie504@student.polsl.pl>
2022-12-21 14:53:44 +0100

    register data passing and redirects

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,29 @@ import { Box, Button } from '@mui/material'
 import React from 'react'
 //import { Button } from './Button'
 import './Login.css'
+import axios, {isCancel, AxiosError} from 'axios';
+
+function handleSubmit() {
+  axios.post("http://127.0.0.1:5000/Register", {
+    email: document.getElementById("email").value,
+    username: document.getElementById("username").value, 
+    password: document.getElementById("password").value
+  }).then((response) => {setTimeout(redirect(response.data.msg), 1000)})
+    .catch((error) => console.error('[FAIL] :: ' + error))
+}
+
+function redirect(error)
+{
+	console.log(error)
+	if(error=='THIS_IS_THE_CORRECT_MESSEGE')
+	{
+		window.location.href = "/Profile"
+	}
+	else
+	{
+		window.alert("somthing wong")
+	}
+}
 
 function Login() {
   return (
@@ -10,9 +33,9 @@ function Login() {
         <form method="POST">
             <Box className='sign-up-form'>
                 <Typography className='typography' variant='h2'>Login</Typography>
-                <TextField margin='normal' type={'email'} variant='outlined' placeholder='Email'/>
-                <TextField margin='normal' type={'password'} variant='outlined'placeholder='Password'/>
-                <Button variant='contained' color='warning' style={{
+                <TextField margin='normal' id="username" type={'username'} variant='outlined' placeholder='Username'/>
+                <TextField margin='normal' id="password" type={'password'} variant='outlined'placeholder='Password'/>
+                <Button onClick={handleSubmit} variant='contained' color='warning' style={{
 								backgroundColor: "#e64381",
 							}}>Login</Button>
             </Box>
--- src/components/Register.js
+++ src/components/Register.js
@@ -9,26 +9,39 @@ import { TimesOneMobiledataRounded } from '@mui/icons-material';
 
 function handleSubmit() {
   axios.post("http://127.0.0.1:5000/Register", {
-    email: "test_hehe@g.pl", 
-    username: "test_hehe", 
-    password: "test"
-  }).then((response) => { console.log("SUCCESS") })
+    email: document.getElementById("email").value,
+    username: document.getElementById("username").value, 
+    password: document.getElementById("password").value
+  }).then((response) => {setTimeout(redirect(response.data.msg), 1000)})
     .catch((error) => console.error('[FAIL] :: ' + error))
+}
+
+//amazing
+function redirect(error)
+{
+	console.log(error)
+	if(error=='register successful')
+	{
+		window.location.href = "/Login"
+	}
+	else
+	{
+		window.alert("something wong")
+	}
 }
 
 function Register() {
   return (
     <div>
-        <form method="POST" onSubmit={handleSubmit}>
+        <form method="POST">
             <Box className='sign-up-form'>
                 <Typography className='typography' variant='h2'>Sign-up</Typography>
-                <TextField margin='normal' name='email' type={'email'} variant='outlined' placeholder='Email'/>
-                <TextField margin='normal' name='username' type={'text'} variant='outlined' placeholder='Username'/>
-                <TextField margin='normal' name='password' type={'password'} variant='outlined'placeholder='Password'/>
-                <button type="button" onClick={handleSubmit}>Submit button 1</button>
-                <Button variant='contained' color='warning' style={{
+                <TextField margin='normal' id="email" name='email' type={'email'} variant='outlined' placeholder='Email'/>
+                <TextField margin='normal' id="username" name='username' type={'text'} variant='outlined' placeholder='Username'/>
+                <TextField margin='normal' id="password" name='password' type={'password'} variant='outlined'placeholder='Password'/>
+                <Button onClick={handleSubmit} variant='contained' color='warning' style={{
 								  backgroundColor: "#e64381",
-							  }}>Sign-up button 2</Button>
+							  }}>Register</Button>
             </Box>
         </form>
     </div>
