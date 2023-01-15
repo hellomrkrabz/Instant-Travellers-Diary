f86665036cc58b5a9e4b7d73675890af89c1c97a
Anna Pogorzelska <65682192+hellomrkrabz@users.noreply.github.com>
2023-01-15 17:11:39 +0100

    changed events table

--- backend/flaskr/event.py
+++ backend/flaskr/event.py
@@ -5,12 +5,29 @@ from datetime import datetime
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.Text)
-    body_html = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.Text)
+    description = db.Column(db.Text)
+    journey_id = db.Column(db.Integer, db.ForeignKey('journeys.id'))
     stage_id = db.Column(db.Integer, db.ForeignKey('stages.id'))
-    # images = db.relationship('Images',
-    #                          backref='event',
-    #                          lazy='dynamic',
-    #                          cascade="all, delete")
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Stage %r>' % self.id
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_journey_id(self):
+        return self.journey_id
+
+    def get_stage_id(self):
+        return self.journey_id
+
+    def get_timestamp(self):
+        return self.timestamp
