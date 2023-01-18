b28649bf5950cf24358f1533673744effce6cf9c
jakubklimczak <kubaklim2001@gmail.com>
2023-01-18 20:59:12 +0100

    Working Maps pins (longitude and latitude =1 for now)

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -5,22 +5,13 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { compose, withProps } from "recompose"
 
-var markers=[{}];
+var markers=[];
 
 function assignMarkers(mar){
-  console.log(mar);
-  if(mar.length==0){
-    markers.push({id: 0,
-      lat: 0.0,
-      lng: 0.0,
-    });
-  }
-  console.log("tutaj");
-  for(var i=0; i<mar.length; i++){
-    console.log(parseFloat(mar[i].lat));
+  for(var i=0; i<mar.events.length; i++){
     markers.push({id: i,
-      lat: parseFloat(mar[i].lat),
-      lng: parseFloat(mar[i].lng)
+      lat: parseFloat(mar.events[i].lat),
+      lng: parseFloat(mar.events[i].lng)
     });
   }
 }
@@ -28,24 +19,13 @@ function assignMarkers(mar){
 async function awaiting(){ 
 const res = await fetch("http://localhost:3000/api/Events/1")
   const resJson = await res.json()
-  console.log(resJson);
   assignMarkers(resJson);
 }
-
-/*
-async function awaiting(){ 
-await fetch(`http://localhost:3000/api/Events/1`, {//adres do zmiany
-        method: "GET",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify()
-      }).then((marker)=> console.log(marker));
-}
-*/
 
 
 const GoogleMaps = () => {
   awaiting();
-  console.log(markers);
+  //console.log(markers);
 const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
