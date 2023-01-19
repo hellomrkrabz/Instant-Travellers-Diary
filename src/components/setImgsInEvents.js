dee7ef431c3666767b790954db3595aefd567035
Zuucker <72306356+Zuucker@users.noreply.github.com>
2023-01-19 10:29:27 +0100

    fixed adding images again

--- src/components/setImgsInEvents.js
+++ src/components/setImgsInEvents.js
@@ -1,26 +1,19 @@
 export async function setImgsInEvents(eventId)
 {
 	var url = document.URL;
-	
-	console.log(eventId);
-	
+		
 	var images=[];
 	
 	for(var i=0;i<eventId.length;i++)
-	{
-		console.log(eventId[i]);
-		
+	{		
 		const res = await fetch("http://localhost:5000/api/event/"+eventId[i]+"/images");
 		
 		const resJson = await res.json();
-		console.log(resJson.images[0]);
-		images.push(resJson.images[0]);
+		for(var j=0;j<resJson.images.length;j++)
+		{
+			images.push(resJson.images[j]);
+		}
 	}
-		
-	//const res = await fetch("http://localhost:5000/api/"+type+"/"+id+"/images");
-	//const resJson = await res.json();
-	console.log(images);
-
 	return images;
 }
 
