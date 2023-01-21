3e276a13da0562a54354ad67b237d651de81587b
jakubklimczak <kubaklim2001@gmail.com>
2023-01-21 15:20:52 +0100

    Map now displays pins from all events :partly_sunny:
    epic

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -4,21 +4,24 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { compose, withProps } from "recompose"
+import getCookie from "./getCookie"
 
 var markers=[];
 
 function assignMarkers(mar){
-  for(var i=0; i<mar.events.length; i++){
-    markers.push({id: i,
-      lat: parseFloat(mar.events[i].lat),
-      lng: parseFloat(mar.events[i].lng)
+  console.log(mar);
+  for(var i=0; i<mar.ids.length; i++){
+    markers.push({id: parseFloat(mar.ids[i].id),
+      lat: parseFloat(mar.ids[i].lat),
+      lng: parseFloat(mar.ids[i].lng)
     });
   }
 }
 
 async function awaiting(){ 
-const res = await fetch("http://localhost:3000/api/Events/1")
+const res = await fetch("http://localhost:3000/api/EventIds/" + getCookie())
   const resJson = await res.json()
+  console.log(resJson);
   assignMarkers(resJson);
 }
 
@@ -34,7 +37,7 @@ const MyMapComponent = compose(
     mapElement: <div style={{ width: `50%`, height: `50%` }} />,
   }), withScriptjs, withGoogleMap)((props) =>
   <GoogleMap
-    defaultZoom={8}
+    defaultZoom={1}
     defaultCenter={{ lat: parseFloat(markers[0].lat), lng: parseFloat(markers[0].lng) }}
   >              
 
