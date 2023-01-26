af9c066d48c3d792d22cfd3033323c4a0aee3781
Zuucker <72306356+Zuucker@users.noreply.github.com>
2023-01-26 13:13:12 +0100

    fixed trailer

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Button } from './Button';
 import { Link } from "react-router-dom";
-
-const src =
-  "https://youtu.be/acfTRaZtews";
+import  "./trailer.css"
 
 const Trailer = () => {
   return (
   	<>
-  	<div>
+  	<div> 
   	<Link to={`/`}>
         <button className="button-add">GO BACK</button>
     </Link>
     </div>
-    <div>
-    <video controls width="100%">
-      <source src={src} type="video/mp4" />
-      Sorry, your browser doesn't support embedded videos.
-    </video>
-    </div>
+	
+	<div className="video-container">
+		<video className="video-trailer" src = '/videos/trailer.mp4' autoPlay loop muted="false" />
+	</div>
     </>
   );
 };
